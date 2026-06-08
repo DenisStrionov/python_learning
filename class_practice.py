dc45f9f0febbb1ef63c6622667bd22641bfe0c0a
Denis <strionovdenis@gmail.com>
2026-06-08 23:17:12 +0900

    Добавил class_practice

---
+++ class_practice.py
@@ -0,0 +1,38 @@
+# class Person:
+#     name = "Денис"
+#
+# person = Person()
+# print(person.name)
+
+# class Person:
+#
+#     def __init__(self, name):
+#         self.name = name
+#
+# person = Person("Денис")
+# print(person.name)
+
+class Cat:
+    pass
+
+barsik = Cat()
+murzik = Cat()
+
+print(barsik)
+print(murzik)
+
+class Dog:
+    pass
+
+sharik = Dog()
+bobik = Dog()
+
+print(sharik)
+print(bobik)
+
+class Book:
+    pass
+book1 = Book()
+book2 = Book()
+print(book1)
+print(book2)
